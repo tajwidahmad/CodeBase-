c565218078868f040c0a57c220623527a1d22cbd
Tajwid Ahmad <tajwidahmad5@gmail.com>
2026-08-17 20:31:30 +0530

    Java_Script

---
+++ Java_Script/Part_one/app.js
@@ -0,0 +1,8 @@
+//? var a = 10;
+//! duplicte values can be implemented again and again like var a=12 && var a=12 && this variable created with var is stored in window itself;
+
+//?const t = "Tajwid Ahmad";
+//! const keyword remains all the same...
+
+//*initilaize -> giving first value to a variable
+
